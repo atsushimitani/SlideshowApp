22351606e9a63a3af195f632f10e50223340c5e3
Atsushi Mitani <tech.mitani@gmail.com>
2018-10-27 21:26:22 +0900

    再生機能実装中

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -12,21 +12,51 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
     let images: [String] = ["image_1.jpg", "image_2.jpg", "image_3.jpg"]
     
     var imageIndex: Int = 0
+    
+    // 自動再生中かどうか
+    var isAutoPlaying: Bool = false
+    
+    // 自動再生用タイマー
+    var autoPlayTimer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = UIImage(named: images[imageIndex])
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func next(_ sender: Any) {
+        showNextImage()
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        showPreviousImage()
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        if (isAutoPlaying) {
+            autoPlayTimer.invalidate()
+            isAutoPlaying = false
+        } else {
+            autoPlayTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(showNextImage), userInfo: nil, repeats: true)
+            isAutoPlaying = true
+        }
+    }
+    
+    /// 次の画像を表示
+    @objc private func showNextImage() {
         if imageIndex == 2 {
             imageIndex = 0
         } else {
@@ -35,7 +65,8 @@ class ViewController: UIViewController {
         imageView.image = UIImage(named: images[imageIndex])
     }
     
-    @IBAction func back(_ sender: Any) {
+    /// 前の画像を表示
+    private func showPreviousImage() {
         if imageIndex == 0 {
             imageIndex = 2
         } else {
@@ -43,9 +74,5 @@ class ViewController: UIViewController {
         }
         imageView.image = UIImage(named: images[imageIndex])
     }
-    
-    @IBAction func play(_ sender: Any) {
-    }
-    
 }
 
