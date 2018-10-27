bc91a162250bc085fa9958d3cec0a3a3b2bd5ccf
Atsushi Mitani <tech.mitani@gmail.com>
2018-10-27 20:17:59 +0900

    画像をアップロード、初期画像を設定

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = UIImage(named: "image_1.jpg")
     }
 
     override func didReceiveMemoryWarning() {
